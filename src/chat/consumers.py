21002bdcf82fa3bfc4370f5d042e1274a8d6de3a
shoya-koyama <rodceaser17@gmail.com>
2024-09-04 23:13:58 +0900

    GPTでテキスト変換システム修正

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -14,40 +14,55 @@ OPENAI_API_KEY = settings.OPENAI_API_KEY
 OPENAI_API_BASE = settings.OPENAI_API_URL
 
 class ChatConsumer(WebsocketConsumer):
+    # WebSocket接続時に呼ばれるメソッド
     def connect(self):
+        # URLからルーム名を取得
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        # ルームグループ名を設定
         self.room_group_name = "chat_%s" % self.room_name
-        # Join room group
+        # ルームグループに参加
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name, self.channel_name
         )
+        # WebSocket接続を受け入れる
         self.accept()
 
+    # WebSocket切断時に呼ばれるメソッド
     def disconnect(self, close_code):
-        # Leave room group
+        # ルームグループから退出
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name, self.channel_name
         )
 
-    # Receive message from WebSocket
+    # WebSocketからメッセージを受信した時に呼ばれるメソッド
     def receive(self, text_data):
+        # 受信したメッセージをJSON形式にデコード
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         
-        chat = ChatOpenAI(openai_api_key=OPENAI_API_KEY, openai_api_base=OPENAI_API_BASE, model_name='gpt-4o-mini', temperature=0)
-        content = f"「{message}」をポジティブな絵文字に変換してください。"
+        # ChatGPT APIを使用して、メッセージをポジティブな絵文字に変換
+        chat = ChatOpenAI(
+            openai_api_key=OPENAI_API_KEY, 
+            openai_api_base=OPENAI_API_BASE, 
+            model_name='gpt-4o-mini', 
+            temperature=0
+        )
+        content = f"「{message}」がポジティブなテキストであれば、そのままにしてください。「{message}」がネガティブなテキストであれば、ポジティブな言葉のみに変換してください。"
         messages = [
             HumanMessage(content=content),
         ]
         
+        # ChatGPT APIからの応答を取得
         result = chat(messages)
-        # Send message to room group
+        
+        # 変換されたメッセージをルームグループに送信
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name, {"type": "chat_message", "message": result.content}
         )
 
-    # Receive message from room group
+    # ルームグループからメッセージを受信した時に呼ばれるメソッド
     def chat_message(self, event):
+        # ルームグループからのメッセージを取得
         message = event["message"]
-        # Send message to WebSocket
+        # WebSocketにメッセージを送信
         self.send(text_data=json.dumps({"message": message}))
