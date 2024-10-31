233f4cc1ee2132b604bcfc5b4fc46cc739f8cf78
shoya-koyama <rodceaser17@gmail.com>
2024-10-31 17:38:31 +0900

    superuser_add

--- src/config/settings.py
+++ src/config/settings.py
@@ -95,17 +95,27 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = { 
-    'default': { 
-        'ENGINE': 'django.db.backends.postgresql', 
-        'NAME': 'testdb', 
-        'USER': 'iniad', 
-        'PASSWORD': 'password', 
-        'HOST': 'db', 
-        'PORT': 5432, 
-    } 
-}
+# DATABASES = { 
+#     'default': { 
+#         'ENGINE': 'django.db.backends.postgresql', 
+#         'NAME': 'testdb', 
+#         'USER': 'iniad', 
+#         'PASSWORD': 'password', 
+#         'HOST': 'db', 
+#         'PORT': 5432, 
+#     } 
+# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DB', default='testdb'),
+        'USER': env('POSTGRES_USER', default='iniad'),
+        'PASSWORD': env('POSTGRES_PASSWORD', default='password'),
+        'HOST': env('POSTGRES_HOST', default='db'),
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
