22d7ece0c58e499761f1767aa7151bd7f535402d
shoya-koyama <rodceaser17@gmail.com>
2024-11-10 22:05:09 +0900

    render

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,25 @@
+# #!/bin/bash
+
+# # エラーが発生した場合にスクリプトを終了する
+# set -e
+
+# # データベースの準備を待機
+# echo "Waiting for database to be ready..."
+# until python ./src/manage.py showmigrations > /dev/null 2>&1; do
+#     echo "Database is unavailable - sleeping"
+#     sleep 3
+# done
+
+# # マイグレーションの実行
+# # echo "Running database migrations..."
+# # python ./src/manage.py migrate --noinput
+
+# python manage.py collectstatic --no-input
+# python manage.py migrate
+# python manage.py superuser
+
+# # daphneサーバーを起動
+# exec daphne -b 0.0.0.0 -p 8000 src.config.asgi:application
 #!/bin/bash
 
 # エラーが発生した場合にスクリプトを終了する
@@ -10,13 +32,13 @@ until python ./src/manage.py showmigrations > /dev/null 2>&1; do
     sleep 3
 done
 
-# マイグレーションの実行
-# echo "Running database migrations..."
-# python ./src/manage.py migrate --noinput
+# 静的ファイルを収集し、マイグレーションを実行
+python ./src/manage.py collectstatic --no-input
+python ./src/manage.py migrate
 
-python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py superuser
+# スーパーユーザーの作成
+python ./src/manage.py superuser
 
-# daphneサーバーを起動
+# Daphneサーバーを起動
 exec daphne -b 0.0.0.0 -p 8000 src.config.asgi:application
+
